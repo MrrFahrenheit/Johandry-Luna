ea26dbc9cd0ececc4768c8f59d8b70e4b57305b7
johandryyasmirluna@gmail.com <147267829+BojackHorsemanyesiambojack@users.noreply.github.com>
2026-03-02 14:28:36 -0600

    2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import TopNavBar from './components/general-view/TopNavBar'
 import { Route, Routes } from 'react-router-dom'
