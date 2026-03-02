1a28b091ecf03f9cb085ea6c293d8e55cb8693d3
johandryyasmirluna@gmail.com <147267829+BojackHorsemanyesiambojack@users.noreply.github.com>
2026-03-02 14:45:14 -0600

    fixeed

--- src/page/Projects.tsx
+++ src/page/Projects.tsx
@@ -1,9 +1,14 @@
-import Card from '../components/general-view/ui/Card'
-import SwordIcon from '../assets/images/Sword.png'
+import { useState } from 'react';
+import SwordIcon from '../assets/images/Sword.png';
+import Card from '../components/general-view/ui/Card';
+import type { IProject } from '../models/interfaces/IObjects';
 
 export default function Projects() {
+
+    const [projects, setProjects] = useState<Array<IProject>>([]);
+
     return (
-                <main className="w-full flex flex-col mt-10 gap-6 px-4">
+                <main className="w-screen flex flex-col mt-10 gap-6 px-4 md:px-0">
             <div className='flex flex-col gap-2'>
                 <div className="flex items-center justify-center gap-2">
                     <img src={SwordIcon} className="md:size-8 size-7" />
@@ -11,49 +16,19 @@ export default function Projects() {
                 </div>
                 <div className="h-1 w-20 bg-red-600 mx-auto rounded-full mb-4"></div>
             </div>
-            <div className="grid gap-6 md:grid-cols-2">
-                <Card>
-                    <h3 className="text-xl font-semibold mb-2 dmc-font">👋 About me</h3>
-                    <p className="text-sm text-zinc-300">
-                        Hello! My name is <strong>Johandry Luna</strong>.
-                        I’m a software developer passionate about building
-                        full-stack applications.
-                        <br /><br />
-                        I'm 19 years old and a Practicant Catholic, currently studying
-                        Techonologies Engineering at the <strong>UNAN LEON - SEDE SOMOTO</strong> in Nicaragua.
-                        I love learning new technologies and improving my skills every day.
-                        <br /><br />
-                        I play the guitar and read books in my free time, also im a big fan of video games and theology.
-                        <br /><br />
-                        I love Devil May Cry and Lady Gaga.
-                    </p>
-                </Card>
-
-                <Card>
-                    <h3 className="text-lg font-semibold mb-2">📃 Experience</h3>
-                    <ul className="text-sm text-zinc-300 list-disc list-inside">
-                        <li>React Native FreeLancer Developer (2024 - Current)</li>
-                    </ul>
-                </Card>
-
-                <Card>
-                    <h3 className="text-lg font-semibold mb-2">🛠️ Tech stack</h3>
-                    <ul className="text-sm text-zinc-300 list-disc list-inside">
-                        <li>Frontend: React, TypeScript, Tailwind CSS</li>
-                        <li>Backend: ASP.NET, FastAPI</li>
-                        <li>Databases: SQL, Appwrite</li>
-                        <li>Other: Git, REST APIs, JWT Auth</li>
-                    </ul>
-                </Card>
-
-                <Card>
-                    <h3 className="text-lg font-semibold mb-2">🎯 Goals</h3>
-                    <p className="text-sm text-zinc-300">
-                        Improve system design skills, contribute to open-source,
-                        and build products that feel professional, scalable,
-                        and well-thought-out.
-                    </p>
-                </Card>
+            <div className="grid gap-6 md:grid-cols-3">
+                {projects.length > 0 ? (
+                    projects.map((project) => (
+                        <Card key={project.id}>
+                            <h3 className="text-xl font-semibold mb-2 dmc-font">{project.title}</h3>
+                            <p className="text-sm text-zinc-300">{project.description}</p>
+                        </Card>
+                    ))
+                ) : (
+                    <div className="col-span-full text-center">
+                        <p className="text-sm text-zinc-300">Ningun proyecto disponible.</p>
+                    </div>
+                )}
             </div>
         </main>
 
