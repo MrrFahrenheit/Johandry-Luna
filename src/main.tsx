26835d7bd3205097e6084e0103689e9343933662
johandryyasmirluna@gmail.com <147267829+BojackHorsemanyesiambojack@users.noreply.github.com>
2026-03-02 14:52:15 -0600

    44

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename='/johandry-luna/'>
+  <BrowserRouter basename='/Johandry-Luna/'>
     <App />
   </BrowserRouter>,
 )
