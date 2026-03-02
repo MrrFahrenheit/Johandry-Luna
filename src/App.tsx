51fe4f70024b6a22a043d95f41c1a8108723bc3c
johandryyasmirluna@gmail.com <147267829+BojackHorsemanyesiambojack@users.noreply.github.com>
2026-03-02 14:38:03 -0600

    Little-fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import TopNavBar from './components/general-view/TopNavBar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './page/Home'
+import Projects from './page/Projects'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className='mt-20'>
         <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/projects' element={<Projects />} />
       </Routes>
       </div>
     </>
